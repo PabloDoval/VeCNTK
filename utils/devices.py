0ec3b86bb57dd376d8d75ce2873e38d0e0c933d1
Pablo Alvarez Doval <pablo.doval@gmail.com>
2018-02-15 19:58:30 +0200

    Fixed the environment activation.

--- utils/devices.py
+++ utils/devices.py
@@ -3,11 +3,11 @@ import cntk as cn
 
 
 def set_devices():
-    if (cn.__version__ != '2.3'):
+    if (cn.__version__ != '2.4'):
         raise Exception('[ERROR]: Invalid CNTK Version')
     all_devices = device.all_devices()
     if all_devices[0].type() == device.DeviceKind.GPU:
-        print('[INFO]: You computer''s GPU suport CUDA acceleration')
+        print('[INFO]: You computer''s GPU does suport CUDA acceleration')
         device.try_set_default_device(device.gpu(0))
     else:
         print('[WARNING]: You computer''s GPU does not suport CUDA acceleration')
